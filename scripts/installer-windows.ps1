250d18f3a234553ce77212115620f6d92fa4302f
letwir <letwirwinkuru@gmail.com>
2026-04-15 12:38:13 +0900

    Update installer-windows.ps1
    cargo-binstall

--- scripts/installer-windows.ps1
+++ scripts/installer-windows.ps1
@@ -9,7 +9,7 @@ rustup default nightly
 cd
 curl https://raw.githubusercontent.com/letwir/config/refs/heads/main/rustup/cargo-windows.toml  -o \.cargo\config.toml
 
-curl https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash -s -- -y
+Set-ExecutionPolicy Unrestricted -Scope Process; iex (iwr "https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.ps1").Content
 curl -sS https://starship.rs/install.sh | sh
 cargo binstall git-delta bat sd vivid cargo-cache
 cargo install fd-find lsd frs ripgrep du-dust hexyl choose lms starship --all-features
